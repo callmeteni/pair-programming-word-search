8c30b86de5bb7a2439036d97ab02c828ff8e81e0
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-06-21 08:19:52 +0000

    updated the wordSearch function so it passes the test

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    // Broken  // const verticalJoin = letters[0].map(ms => ms.join(''));
+    // const verticalJoin = letters[0].map((col, i) => letters.map(column => column[i]).join(''));
+    const verticalJoin = letters[0].map((col, i) => letters.map(col => col[i]).join(''));
+
+    for (const l of horizontalJoin) {
+        for (const col of verticalJoin) {
+            if (col.includes(word)) {
+                return true;
+            }
+        }
+        if (l.includes(word)) {
+            return true;
+        }
     }
-}
+    return false;
+};
+
 
 module.exports = wordSearch
